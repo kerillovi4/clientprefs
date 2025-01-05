24fc3e2619dc0ac170ff5309fea5922b4e26a9df
kerillovi4@gmail.com <kerillovi4@gmail.com>
2025-01-05 23:12:10 +0300

    Добавьте файлы проекта.

---
+++ clientprefs/Main.cs
@@ -0,0 +1,20 @@
+﻿using Plugify;
+using static s2sdk.s2sdk;
+
+namespace clientprefs
+{
+    public unsafe class Main : Plugin
+    {
+        public void OnPluginStart()
+        {
+            OnClientPutInServer_Register(OnClientPutInServerCallback);
+        }
+
+        private void OnClientPutInServerCallback(int clientIndex)
+        {
+            ulong accountId = GetClientAccountId(clientIndex);
+
+            PrintToServer(string.Format(">>>> {0}", accountId.ToString()));
+        }
+    }
+}
